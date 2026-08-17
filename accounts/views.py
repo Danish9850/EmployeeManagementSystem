02f4ca69782617cc9ecf5173f59ceddadc71e6cf
Mohammad <mdudanish313@gmail.com>
2026-08-17 10:36:04 +0530

    Add user signup feature

--- accounts/views.py
+++ accounts/views.py
@@ -8,7 +8,8 @@ from django.contrib import messages
 def home(request):
     return render(request, 'index.html')
 
-# User Signup Function
+
+# User Signup
 def signup(request):
     if request.method == 'POST':
 
@@ -16,79 +17,52 @@ def signup(request):
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
 
-        # Check password
         if password != confirm_password:
             messages.error(request, "Passwords do not match")
-            return render(request,'signup.html')
+            return render(request, 'signup.html')
 
-        # check username already exists
         if User.objects.filter(username=username).exists():
             messages.error(request, "Username already exists")
             return render(request, 'signup.html')
 
-        # Create User
-        user = User.objects.create_user(
+        User.objects.create_user(
             username=username,
             password=password
         )
 
-        messages.success(request, "Account created successfully. Please login.")
-        return redirect('login')
-    return render(request, 'signup.html')
-
-# User Login Function
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = authenticate(
-            request,username=username,
-            password=password
+        messages.success(
+            request,
+            "Account created successfully. Please login."
         )
 
-        if user:
-            login(request, user)
-            return redirect("employee_list")
+        return redirect('login')
 
-        else:
-            messages.error(request, "Invalid Username or Password")
-            return render(request,'login.html')
+    return render(request, 'signup.html')
 
-# User Logout Function
-def user_logout(request):
-        logout(request)
-        return redirect
 
-# User Login Function
+# User Login
 def user_login(request):
-
-    # Check if form submitted
     if request.method == 'POST':
 
-        # Get username and password from form
         username = request.POST['username']
         password = request.POST['password']
 
-        # Authenticate user
         user = authenticate(
             request,
             username=username,
             password=password
         )
 
-        # If credentials are correct
         if user:
-            login(request, user)            
+            login(request, user)
             return redirect('employee_list')
-        else:
-            messages.error(request, "Invalid Username or Password")
 
-    
+        messages.error(request, "Invalid Username or Password")
+
     return render(request, 'login.html')
 
 
-# User Logout Function
+# User Logout
 def user_logout(request):
     logout(request)
     return redirect('login')
